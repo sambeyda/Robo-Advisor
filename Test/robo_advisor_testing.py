c0cc4845dc9e3ca72e24f3a298643ba29c6c1fce
Sam Beyda <sb1629@georgetown.edu>
2019-05-06 20:45:42 -0400

    Configure travis, and past USD test

---
+++ Test/robo_advisor_testing.py
@@ -0,0 +1,14 @@
+#Robo Advisor Testing
+import os
+
+from App.robo_advisor import to_usd
+
+def test_to_usd(): #Taken from executive dashboard testing
+    #Tests whether number is returned with $ sign and two decimal places
+    assert to_usd(10) == "$10.00"
+
+    #Tests whether number rounds decimal places
+    assert to_usd(10.33333333333333) == "$10.33"
+
+    #Tests whether there is a thousand seperator
+    assert to_usd(10000) == "$10,000.00"
